796619c1ab210c8b095e8ce1863674e12a037423
Гущин Андрей Леонидович <andrey67895@gmail.com>
2025-12-15 02:18:35 +0500

    rag bot

--- task4/rag_bot.py
+++ task4/rag_bot.py
@@ -11,7 +11,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 class LocalLLM:
 
 
-    def __init__(self, model_name="tiiuae/falcon-7b-instruct", device="cpu"):
+    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2", device="cpu"):
         if device == "cuda" and torch.cuda.is_available():
             device_map = "auto"
             torch_dtype = torch.float16  # Используем половинную точность для экономии памяти
@@ -55,7 +55,7 @@ class LocalLLM:
 
 # Класс RAG-бота
 class RAGBot:
-    def __init__(self, faiss_index_path, embedding_model="all-MiniLM-L6-v2", llm_model="tiiuae/falcon-7b-instruct"):
+    def __init__(self, faiss_index_path, embedding_model="all-MiniLM-L6-v2", llm_model="sentence-transformers/all-MiniLM-L6-v2"):
         # Эмбеддинги
         self.embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
         # Загрузка FAISS индекса
