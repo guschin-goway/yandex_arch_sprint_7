7bc309273c66f28066ac11dba2f46eb0ecdf0ecb
Гущин Андрей Леонидович <andrey67895@gmail.com>
2025-12-15 01:56:56 +0500

    rag bot

--- task4/rag_bot.py
+++ task4/rag_bot.py
@@ -1,6 +1,6 @@
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
